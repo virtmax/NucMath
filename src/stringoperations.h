04b35b5eb02686236fa0e90b6133c783c0ce9770
Maxim Singer <virtmax@users.noreply.github.com>
2016-11-29 18:14:57 +0100

    add to_string function

--- src/stringoperations.h
+++ src/stringoperations.h
@@ -1,6 +1,10 @@
 
 #include <vector>
+#include <string>
+#include <sstream>
+#include <iomanip>
 
+#pragma once
 
 namespace nucmath
 { 
@@ -9,4 +13,11 @@ namespace nucmath
 	std::vector<std::string> tokenize(const std::string& str, std::vector<char> delims);
 
 
+    template <typename T>
+    std::string to_string(const T a_value, const int n = 3)
+    {
+        std::ostringstream out;
+        out << std::fixed << std::setprecision(n) << a_value;
+        return out.str();
+    }
 }

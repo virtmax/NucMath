b655d277a7b7b2cd4fc10f362d55c78bcefb4634
Maxim Singer <virtmax@users.noreply.github.com>
2018-05-14 10:38:08 +0200

    utf-8 fix

--- src/functions.h
+++ src/functions.h
@@ -1,7 +1,4 @@
 
-
-
-
 #include <array>
 #include <vector>
 #include <random>
@@ -13,61 +10,57 @@
 
 namespace nucmath
 {
+/*
+    Model: A*exp(-(x-µ)^2 / (2*sigma^2)) / sqrt(2*Pi*sigma^2)
+
+    p - Array with model parameters
+    0 = A
+    1 = µ
+    2 = sigma
+
+    x - parameter
+
+*/
+double normal_dist_pdf(const std::array<double, 3> &p, double x);
+
+// x = ableseort, mu = zentrum position, sigma = standardabweichung
+double normal_dist_pdf(double mu, double sigma, double x);
+
+// Summe aus zwei normal_dist_pdf
+double normal_dist2_pdf(const std::array<double, 6> &p, double x);
+
+double normal_dist_2d_pdf(const std::array<double, 6> &p, double x1, double x2);
+
+double normal_dist_intersection_point(double mu1, double sigma1, double A1, double mu2, double sigma2, double A2);
 
 
-	/*
-	Model: A*exp(-(x-µ)^2 / (2*sigma^2)) / sqrt(2*Pi*sigma^2)
+/*
+        p - Array with model parameters
+        0 - Energy (Temperature)
 
-	p - Array with model parameters
-	0 = A
-	1 = µ
-	2 = sigma
-
-	x - parameter
-
-	*/
-	double normal_dist_pdf(const std::array<double, 3> &p, double x);
-
-	// x = ableseort, mu = zentrum position, sigma = standardabweichung
-	double normal_dist_pdf(double mu, double sigma, double x);
-
-	// Summe aus zwei normal_dist_pdf
-	double normal_dist2_pdf(const std::array<double, 6> &p, double x);
-
-    double normal_dist_2d_pdf(const std::array<double, 6> &p, double x1, double x2);
-
-
-	double normal_dist_intersection_point(double mu1, double sigma1, double A1, double mu2, double sigma2, double A2);
-
-
-
-	/*
-	p - Array with model parameters
-	0 - Energy (Temperature)
-
-	x - variable
-	*/
-	double maxwell_boltzmann_dist_pdf(const std::array<double, 1> &p, double x);
+        x - variable
+        */
+double maxwell_boltzmann_dist_pdf(const std::array<double, 1> &p, double x);
 
 
 
-	/*
-	p - Array with model parameters
-	0 - Energy (Temperature)
-	1 - Watt-Parameter
+/*
+        p - Array with model parameters
+        0 - Energy (Temperature)
+        1 - Watt-Parameter
 
-	x - variable
-	*/
-	double watt_dist_pdf(const std::array<double, 2> &p, double x);
-
-
-
-	// TODO: auf gcc kompilier nicht thread-sicher !
-	double getRandom(double start, double end);
+        x - variable
+        */
+double watt_dist_pdf(const std::array<double, 2> &p, double x);
 
 
-	int sign(double x);
 
-    double rad2deg(double angle_rad);
-    double deg2rad(double angle_deg);
+// TODO: auf gcc kompilier nicht thread-sicher !
+double getRandom(double start, double end);
+
+
+int sign(double x);
+
+double rad2deg(double angle_rad);
+double deg2rad(double angle_deg);
 }
--- src/interpolation.h
+++ src/interpolation.h
@@ -8,30 +8,30 @@
 
 namespace nucmath
 {
-	enum InterpolationMode { Linear = 0, Steffen };
+enum InterpolationMode { Linear = 0, Steffen };
 
-	class Interpolation
-	{
-	public:
-		Interpolation();
-		virtual ~Interpolation();
+class Interpolation
+{
+public:
+    Interpolation();
+    virtual ~Interpolation();
 
-		void interpolate(InterpolationMode mode);
+    void interpolate(InterpolationMode mode);
 
-        void setData(const DataTable<2>& data);
-		void setData(const std::vector<double>& data);
+    void setData(const DataTable<2>& data);
+    void setData(const std::vector<double>& data);
 
-	private:
+private:
 
-        DataTable<2> m_dataTable;
-		std::vector<std::pair<double, double>> m_data;
+    DataTable<2> m_dataTable;
+    std::vector<std::pair<double, double>> m_data;
 
-		InterpolationMode m_mode;
+    InterpolationMode m_mode;
 
 
-		std::vector<std::pair<double,double>> LinearInterpolation();
+    std::vector<std::pair<double,double>> LinearInterpolation();
 
-		std::vector<std::pair<double,double>> SteffenInterpolation();
-	};
+    std::vector<std::pair<double,double>> SteffenInterpolation();
+};
 
 }
